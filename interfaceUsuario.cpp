6eb37ef7ab5ff138185d794af84fb3b0f2dfa9d1
wallas2022 <116705801+wallas2022@users.noreply.github.com>
2024-04-01 09:52:20 -0600

    se agrego espera en cada opcion y salir

--- interfaceUsuario.cpp
+++ interfaceUsuario.cpp
@@ -1,39 +1,50 @@
 #include "clases/interfaceUsuario.h"
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
 
 void InterfaceUsuario::menuPrincipal() {
      int opcion = 0;
     do {
-        std::cout << "\nMenu Principal\n";
-        std::cout << "1. Cargar archivo de traducción\n";
-        std::cout << "2. Traducir\n";
-        std::cout << "3. Agregar Traducción\n";
-        std::cout << "4. Eliminar traducción\n";
-        std::cout << "5. Salir\n";
-        std::cout << "Seleccione una opción: ";
-        std::cin >> opcion;
+        cout << "\nMenu Principal\n";
+        cout << "1. Cargar archivo de traducción\n";
+        cout << "2. Traducir\n";
+        cout << "3. Agregar Traducción\n";
+        cout << "4. Eliminar traducción\n";
+        cout << "5. Salir\n";
+        cout << "Seleccione una opción: ";
+        cin >> opcion;
 
         switch (opcion) {
             case 1:
-                std::cout << "Cargando archivo de traducción...\n";
+                cout << "Cargando archivo de traducción...\n";
+                getch(); 
                 // Lógica para cargar archivo
                 break;
             case 2:
-                std::cout << "Traduciendo...\n";
+                cout << "Traduciendo...\n";
+                getch(); 
                 // Lógica para traducir
                 break;
             case 3:
-                std::cout << "Agregando traducción...\n";
+                cout << "Agregando traducción...\n";
+                getch(); 
                 // Lógica para agregar traducción
                 break;
             case 4:
-                std::cout << "Eliminando traducción...\n";
+                cout << "Eliminando traducción...\n";
+                getch(); 
                 // Lógica para eliminar traducción
                 break;
             case 5:
-                std::cout << "Saliendo...\n";
+                getch(); 
+                cout << "Saliendo...\n";
+            
                 break;
             default:
-                std::cout << "Opción no válida. Por favor, intente nuevamente.\n";
+                cout << "Opción no válida. Por favor, intente nuevamente.\n";
+                getch(); 
                 break;
         }
     } while (opcion != 5);
